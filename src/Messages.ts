9e34fba418d7b47c4dc5ebc81a27e877e8a4ab03
bktsim <brendontsim@gmail.com>
2022-10-11 01:08:19 -0700

    newText

--- src/Messages.ts
+++ src/Messages.ts
@@ -2,12 +2,12 @@ import LocalizedStrings from 'react-localization';
 
 const strings = new LocalizedStrings({
     en: {
-        name: "brendon tsim",
-        subline: "software development",
+        name: "Brendon Tsim",
+        subline: "Software Development",
 
-        github: "github",
-        resume: "resume",
-        email: "email",
+        github: "GitHub",
+        resume: "Resume",
+        email: "Email",
 
         intro_first: `Hi! I'm Brendon, a Computer Science student at The University of British Columbia in Vancouver, BC, Canada.`,
         intro_second: `My interests in Computer Science lie in programming languages and distributed computing.`,
@@ -15,7 +15,7 @@ const strings = new LocalizedStrings({
         intro_fourth: `I also like to apply new knowledge to my interests by working on game development & livestreaming related projects.`,
         intro_fifth: `For academic, career opportunities or other enquiries, please contact me through my email.`,
 
-        footer: 'background image by Laurence Lee',
+        footer: 'Background image by Laurence Lee',
     },
     zh: {
         name: "詹耀鈞",
@@ -26,7 +26,7 @@ const strings = new LocalizedStrings({
         email: "電郵",
 
         intro_first: `您好！我是耀鈞 (Brendon), 一位在英屬哥倫比亞大學裏修讀計算機科學的大學生。`,
-        intro_second: `在計算機科學的領域裏，我對程式語言和分布式計算這兩個領域特別感興趣。`,
+        intro_second: `在計算機科學的領域裏，我對程式語言、分布式計算和機器學習這三個領域特別感興趣。`,
         intro_third: `在我空閑的時間，我喜歡彈吉他，追蹤有關我買不起的鍵盤的資訊和學習日文。`,
         intro_fourth: `我也喜歡透過研發游戲以及有關直播的科技去把學到的新知識應用到我的興趣裏。`,
         intro_fifth: `有關學術，工作的機會或其他查詢，請透過電郵聯絡我。`,
@@ -42,9 +42,9 @@ const strings = new LocalizedStrings({
         email: "メール",
 
         intro_first: `初めまして！カナダのブリティッシュコロンビア大学、コンピューターサイエンス専攻のブレンドン (Brendon) と申します。`,
-        intro_second: `コンピューターサイエンスの分野の中で、プログラミング言語と分散システムには興味があります。`,
+        intro_second: `コンピューターサイエンスの分野の中で、プログラミング言語、分散システムと機械学習には興味があります。`,
         intro_third: `暇な時はギターを引いたり、買えないキーボードを見たり、日本語を勉強したりのが好きです。`,
-        intro_fourth: `ゲームと配信に関するプロジェクトを開発することで知識を用いることも好きです。`,
+        intro_fourth: `ゲームと配信に関するプロジェクトを開発することで学んだ知識を使うのも好きです。`,
         intro_fifth: `学術・仕事の機会やお問い合わせはメールでご連絡くださいますようお願いいたします。`,
 
         footer: '背景画像: Laurence Lee',

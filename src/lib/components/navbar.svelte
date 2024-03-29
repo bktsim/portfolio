2d51e6e1def5ea37412e9cdd407c779395185db8
bktsim <brendontsim@gmail.com>
2024-03-29 02:31:57 -0700

    wip

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -3,20 +3,29 @@
 	 * @type {string}
 	 */
 	export let page;
+
+	const text = {
+		name: 'brendon',
+		about: 'about',
+		projects: 'projects',
+		experience: 'work experience',
+		hobbies: 'hobbies'
+	};
 </script>
 
-<header class="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
+<header
+	class="flex md:sticky md:top-0 inset-x-0 flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm backdrop-blur-sm rounded"
+>
 	<nav
-		class="mt-6 relative max-w-[85rem] w-full bg-white border border-gray-200 rounded-[36px] mx-2 py-3 px-4 md:flex md:items-center md:justify-between md:py-0 md:px-6 lg:px-8 xl:mx-auto dark:bg-gray-800 dark:border-gray-700"
+		class="mt-6 relative max-w-[85rem] w-full bg-white border border-gray-200 rounded-[36px] mx-2 py-3 px-4 md:flex md:flex-row md:items-center md:justify-between md:py-0 md:px-6 lg:px-8 xl:mx-auto dark:bg-gray-800 dark:border-gray-700"
 		aria-label="Global"
 	>
+		<a class="grow hidden md:inline-flex min-w-fit name pl-2" href="/" aria-label="Brand"
+			>{text.name}</a
+		>
 		<div>
 			<div class="md:hidden flex items-center justify-between">
-				<a
-					class="inline-flex grow flex-row text-xl font-semibold dark:text-white"
-					href="/"
-					aria-label="Brand">Brendon Tsim</a
-				>
+				<a class="inline-flex grow flex-row pl-2 name" href="/" aria-label="Brand">{text.name}</a>
 				<button
 					type="button"
 					class="self-end hs-collapse-toggle size-8 flex justify-center items-center text-sm font-semibold rounded-full border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
@@ -62,13 +71,35 @@
 			class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block"
 		>
 			<div
-				class="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:items-center md:justify-center md:gap-y-0 md:gap-x-7 md:mt-0 md:ps-7"
+				class="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:self-end md:justify-end md:gap-y-0 md:gap-x-7 md:mt-0 md:ps-7"
 			>
 				<a
-					class="font-bold text-xl md:py-6 text-gray-500 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
 					class:selected={page === 'index'}
+					class:unselected={page !== 'index'}
+					class="links"
 					href="/"
-					aria-current="page">Home</a
+					aria-current="page">{text.about}</a
+				>
+				<a
+					class="links"
+					class:selected={page === 'experience'}
+					class:unselected={page !== 'experience'}
+					href="/"
+					aria-current="page">{text.experience}</a
+				>
+				<a
+					class="links"
+					class:selected={page === 'projects'}
+					class:unselected={page !== 'projects'}
+					href="/"
+					aria-current="page">{text.projects}</a
+				>
+				<a
+					class="links"
+					class:selected={page === 'fun'}
+					class:unselected={page !== 'fun'}
+					href="/"
+					aria-current="page">{text.hobbies}</a
 				>
 				<!-- <a
 					class="font-semibold text-xl text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500"
@@ -84,5 +115,28 @@
 	.selected {
 		@apply text-blue-600;
 		@apply dark:text-blue-500;
+		@apply font-bold;
+	}
+
+	.unselected {
+		@apply text-gray-500;
+		@apply hover:text-gray-400;
+		@apply dark:text-gray-400;
+		@apply dark:hover:text-gray-500;
+	}
+
+	.links {
+		@apply font-semibold;
+		@apply text-xl;
+		@apply ml-2;
+		@apply md:ml-0;
+		@apply md:py-6;
+	}
+
+	.name {
+		@apply text-3xl;
+		@apply md:text-2xl;
+		@apply font-bold;
+		@apply dark:text-white;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,116 @@
 </script>
 
 <Navbar page="index" />
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="mt-14 md:mt-20 max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
+	<div class="m-8">
+		<div class="flex justify-center min-h-[41vh]">
+			<div class="flex flex-col justify-center">
+				<h1 class="text-5xl md:text-6xl lg:text-7xl xl:text-8xl inline-flex">(´• ω •`)ﾉ</h1>
+			</div>
+			<div class="flex flex-col justify-center ml-6 md:ml-14">
+				<h1 class="text-5xl md:text-6xl xl:text-8xl dark:text-white font-bold">brendon tsim</h1>
+				<h3 class="text-3xl md:text-4xl xl:text-5xl dark:text-white font-medium mt-2">
+					software developer
+				</h3>
+			</div>
+		</div>
+		<article class="md:m-18 md:mt-20">
+			<div>
+				<h4 class="title">about</h4>
+				<div class="paragraph-container">
+					<p class="paragraph">
+						Hey there! I'm Brendon, a software developer and a 4th year undergraduate student at The
+						University of British Columbia in Vancouver, Canada majoring in Computer Science.
+					</p>
+					<p class="paragraph">
+						In my free time, you can catch me playing the guitar, window shopping keyboards that I
+						really shouldn't get, learning Japanese (i.e. watching VTubers) and playing video games
+						- especially FPS and Rhythm games (maimai!). I chose to study computer science because I
+						basically live on the internet. It's as if I'm in sorcery school.
+					</p>
+					<p class="paragraph">
+						I'm on the lookout for cool projects that are related to my interests and full-time
+						roles in Canada, USA, Australia, Japan and Hong Kong. I can speak English, Cantonese,
+						Mandarin and Japanese, so any role using more than one language would be a cool plus!
+					</p>
+					<p class="paragraph">
+						If you'd like to get in touch for academic/career opportunities, projects, or if you'd
+						just like to chat, please dont hesitate to reach out through my <a
+							href="mailto:contact@brendontsim.com?subject=From brendontsim.com | [YOUR SUBJECT HERE]"
+							class="font-bold text-blue-500 underline">email</a
+						>!
+					</p>
+				</div>
+			</div>
+
+			<div class="section">
+				<h4 class="title">computer science</h4>
+				<div class="paragraph-container">
+					<p class="paragraph">
+						In computer science, I'm most interested in distributed systems, parallel computing,
+						computer networks and machine learning. My interests stems from my habit of watching
+						live streams. I love watching streams live, and I want to improve this experience for
+						everyone.
+					</p>
+					<p class="paragraph">
+						Aside from livestreaming-related computer science topics, I'm also interested in
+						Operating Systems, Software Verification, Natural Language Processing (especially speech
+						and information retrieval related applications), and Computer Vision.
+					</p>
+				</div>
+			</div>
+		</article>
+		<hr class="section-divider" />
+		<article class="mt-8">
+			<div class="section">
+				<h4 class="title">work experience</h4>
+				<div class="paragraph-container">
+					<p class="paragraph">
+						In computer science, I'm most interested in distributed systems, parallel computing,
+						computer networks and machine learning. My interests stems from my habit of watching
+						live streams. I love watching streams live, and I want to improve this experience for
+						everyone.
+					</p>
+					<p class="paragraph">
+						Aside from livestreaming-related computer science topics, I'm also interested in
+						Operating Systems, Natural Language Processing (especially speech and information
+						retrieval related applications), and Computer Vision.
+					</p>
+				</div>
+			</div>
+		</article>
+	</div>
+</div>
+
+<style>
+	.title {
+		@apply text-6xl;
+		@apply font-light;
+		@apply mt-14;
+		@apply md:mt-9;
+	}
+
+	.paragraph {
+		@apply text-2xl;
+		@apply text-justify;
+		@apply mt-5;
+	}
+
+	.section-divider {
+		/* @apply border-2; */
+		@apply mt-16;
+		@apply md:mt-12;
+		@apply md:mb-5;
+		@apply mx-8;
+	}
+
+	.paragraph-container {
+		@apply ml-4;
+		@apply mt-6;
+	}
+
+	.section {
+		@apply my-24;
+	}
+</style>

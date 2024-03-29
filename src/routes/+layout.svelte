cdd9378f279e09c0166d47036886fc30cdd53225
bktsim <brendontsim@gmail.com>
2024-03-28 21:49:13 -0700

    feat: gh-pages setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { afterNavigate } from "$app/navigation";
+    import "../app.css";
+
+    afterNavigate(() => {
+        // @ts-ignore
+        window.HSStaticMethods.autoInit();
+    });
+</script>
+  
+<slot />

e894ccdade66a8dac8e50cbc955888e9aa773d2d
bktsim <brendontsim@gmail.com>
2024-03-29 22:38:47 -0700

    remove dark navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -47,7 +47,7 @@
 	class="flex md:sticky md:top-0 inset-x-0 flex-wrap md:justify-start md:flex-nowrap z-50 min-w-screen text-sm backdrop-blur-sm rounded"
 >
 	<nav
-		class="mt-6 relative max-w-[85rem] w-full bg-white border border-gray-200 rounded-[36px] mx-2 py-3 px-4 md:flex md:flex-row md:items-center md:justify-between md:py-0 md:px-6 lg:px-8 xl:mx-auto dark:bg-gray-800 dark:border-gray-700"
+		class="mt-6 relative max-w-[85rem] w-full bg-white border border-gray-200 rounded-[36px] mx-2 py-3 px-4 md:flex md:flex-row md:items-center md:justify-between md:py-0 md:px-6 lg:px-8 xl:mx-auto"
 		aria-label="Global"
 	>
 		<a class="grow hidden md:inline-flex min-w-fit name pl-2" href="{base}/" aria-label="Brand"
@@ -60,7 +60,7 @@
 				>
 				<button
 					type="button"
-					class="self-end hs-collapse-toggle size-8 flex justify-center items-center text-sm font-semibold rounded-full border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+					class="self-end hs-collapse-toggle size-8 flex justify-center items-center text-sm font-semibold rounded-full border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
 					data-hs-collapse="#navbar-collapse-with-animation"
 					aria-controls="navbar-collapse-with-animation"
 					aria-label="Toggle navigation"
@@ -133,11 +133,6 @@
 					href="{base}/hobbies"
 					aria-current="page">{text.hobbies}</a
 				>
-				<!-- <a
-					class="font-semibold text-xl text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500"
-					class:selected={page === 'account'}
-					href="/">Account</a
-				>-->
 			</div>
 		</div>
 	</nav>
@@ -146,15 +141,12 @@
 <style>
 	.selected {
 		@apply text-blue-600;
-		@apply dark:text-blue-500;
 		@apply font-bold;
 	}
 
 	.unselected {
 		@apply text-gray-500;
 		@apply hover:text-gray-400;
-		@apply dark:text-gray-400;
-		@apply dark:hover:text-gray-500;
 	}
 
 	.links {
@@ -169,6 +161,5 @@
 		@apply text-3xl;
 		@apply md:text-2xl;
 		@apply font-bold;
-		@apply dark:text-white;
 	}
 </style>

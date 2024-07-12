8db1429c8ffaca604ea81ed3d3019ccd3fa6b44f
bktsim <46881538+bktsim@users.noreply.github.com>
2024-07-12 21:00:00 +0800

    remove dark mode

--- src/lib/components/project.svelte
+++ src/lib/components/project.svelte
@@ -34,23 +34,23 @@
 <button 
   type="button" 
   disabled={projectBulletpoint.length === 0 && projectLink === ""} 
-  class="flex flex-col grow w-full sm:mx-2 group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 mt-4 md:mt-0" 
+  class="flex flex-col grow w-full sm:mx-2 group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition  mt-4 md:mt-0" 
   data-hs-overlay="#{projectId}"
 >
     <div class="rounded-t-xl w-full">
       <img class="top-0 start-0 group-hover:scale-105 transition-transform duration-500 ease-in-out aspect-video w-full object-cover max-h-[17rem] md:max-h-36" src="{base}/projects/{projectId}.png" alt="Descr">
     </div>
     <div class="p-4 md:p-5 flex flex-col grow w-full text-center">
-      <h3 class="text-lg font-bold text-gray-800 dark:text-white">
+      <h3 class="text-lg font-bold text-gray-800 ">
         {projectName}
       </h3>
       <div class="flex flex-col grow justify-center">
-        <p class="mt-1 text-gray-500 dark:text-neutral-400">
+        <p class="mt-1 text-gray-500 ">
           {projectBlurb}
         </p>
       </div>
       {#if imageCredits !== ""}
-        <p class="text-xs font-light text-gray-500 dark:text-neutral-400 bottom-0 mt-2">
+        <p class="text-xs font-light text-gray-500 bottom-0 mt-2">
           {imageCredits}
         </p>
       {/if}
@@ -60,18 +60,18 @@
 {#if projectBulletpoint.length > 0}
 <div id="{projectId}" class="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none">
     <div class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto h-[calc(100%-3.5rem)] min-h-[calc(100%-3.5rem)] flex items-center">
-      <div class="w-full max-h-full overflow-hidden flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-neutral-800 dark:border-neutral-700 dark:shadow-neutral-700/70">
-        <div class="flex justify-between items-center py-3 px-4 border-b dark:border-neutral-700">
-          <h2 class="text-xl font-bold text-gray-800 dark:text-white inline-flex">
+      <div class="w-full max-h-full overflow-hidden flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto ">
+        <div class="flex justify-between items-center py-3 px-4 border-b ">
+          <h2 class="text-xl font-bold text-gray-800  inline-flex">
             {projectName}
             {#if projectLink !== ""}
             <div class="hs-tooltip inline-flex">
-              <a href={projectLink} target="_blank" class="hs-tooltip-toggle flex flex-col justify-center align-middle ml-2 text-blue-500 dark:text-blue-500">
+              <a href={projectLink} target="_blank" class="hs-tooltip-toggle flex flex-col justify-center align-middle ml-2 text-blue-500 ">
                 <div class="flex flex-col justify-center align-middle w-full font-light hover:underline text-md">
                     <svg fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="size-4 hover:scale-125 transition-transform duration-200 ease-in-out">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
                     </svg>  
-                    <span class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded shadow-sm dark:bg-neutral-700" role="tooltip">
+                    <span class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded shadow-sm " role="tooltip">
                       {tooltipText}
                     </span> 
                   </div>
@@ -79,7 +79,7 @@
             </div>
             {/if}
           </h2>
-          <button type="button" class="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700" data-hs-overlay="#{projectId}">
+          <button type="button" class="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none " data-hs-overlay="#{projectId}">
             <span class="sr-only">Close</span>
             <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <path d="M18 6 6 18"></path>

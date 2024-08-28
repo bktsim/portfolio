0b02e3111d3b74fbbdacf38b0e3b5ebaace64484
bktsim <46881538+bktsim@users.noreply.github.com>
2024-08-28 18:02:51 +0800

    Updates

--- _app/immutable/nodes/10.DrGWZJjU.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"../chunks/paths.CrfocpzT.js";import{r as o}from"../chunks/index.De_1-CT6.js";function r(){o(302,`${e}/hello`)}const l=Object.freeze(Object.defineProperty({__proto__:null,load:r},Symbol.toStringTag,{value:"Module"}));export{l as universal};

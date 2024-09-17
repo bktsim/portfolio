2c2e130a1429c09d0448ce3027cd19cfe2fef269
bktsim <46881538+bktsim@users.noreply.github.com>
2024-09-17 12:14:43 +0800

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,74 @@
+<main>
+    <!-- <Navbar /> -->
+    <div class="grid grid-cols-7 pt-10">
+        <div class="col-span-7 md:col-span-3 flex justify-center">
+            <!-- <pre class="text-[0.0525em]/[1.06em]">{PFP}</pre> -->
+        </div>
+        <div class="col-span-7 pt-4 md:pt-0 md:col-span-4">
+            <div
+                class="md:p-4 h-full grow flex flex-col justify-center"
+            >
+                <h3>hal</h3>
+                <p>permanent resident of the interwebs</p>
+                <ul class="list">
+                    <li>
+                        <a href="https://x.com/halwowo" target="_blank">
+                            x.com
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="grid grid-cols-6 gap-6">
+        <div class="col-span-6 box">
+            <h4>about</h4>
+            <p>incoming swe at some series-A startup in japan.</p>
+            <p>
+                interested in distsys, networks, databases, applied ml
+                and entreprenurship.
+            </p>
+        </div>
+
+        <div class="col-span-6 md:col-span-3 box">
+            <h4>recs</h4>
+            <ul class="list">
+                <li>
+                    <a
+                        href="https://myanimelist.net/anime/57524/Make_Heroine_ga_Oosugiru"
+                        target="_blank">too many losing heroines</a
+                    >
+                </li>
+                <li>
+                    vspo (<a
+                        class="inline"
+                        href="https://myanimelist.net/anime/57524/Make_Heroine_ga_Oosugiru"
+                        target="_blank">hinano</a
+                    >,
+                    <a
+                        href="https://myanimelist.net/anime/57524/Make_Heroine_ga_Oosugiru"
+                        target="_blank">met</a
+                    >,
+                    <a
+                        href="https://myanimelist.net/anime/57524/Make_Heroine_ga_Oosugiru"
+                        target="_blank">tsuna</a
+                    >)
+                </li>
+                <li>
+                    <a
+                        href="https://15445.courses.cs.cmu.edu/fall2023/"
+                        target="_blank">CMU 15-445 database systems</a
+                    >
+                </li>
+            </ul>
+        </div>
+        <div class="col-span-6 md:col-span-3 box">
+            <h4>agenda</h4>
+            <ul class="list">
+                <li>databases deep dive</li>
+                <li>job board project</li>
+                <li>job prep</li>
+            </ul>
+        </div>
+    </div>
+</main>

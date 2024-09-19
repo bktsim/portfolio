99247d3d1620e3f383894b4efc544e4fc89a6ae5
bktsim <46881538+bktsim@users.noreply.github.com>
2024-09-20 00:25:49 +0800

    resources page

--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -1,24 +1,40 @@
 <script>
     import Navbar from "$lib/Navbar.svelte";
+
+    import { BOOKS_PFP } from "../../resource.js";
 </script>
 
 <main>
     <Navbar />
     <h4>reading list</h4>
-    <p>
-        My list of resources that I have used, or actively using here to learn
-        more about topics I'm interested in. Mainly CS with some others at
-        times. Also includes articles or cool reads that I may want to reference
-        at times. I want to keep learning cool things.
-    </p>
-    <p>
-        Not listed in order of interest or things to work on, but anything that
-        I'm actively working on has a WIP tag attached to it. Anything that I
-        haven't gone through has a TODO tag attached. Working on going through
-        it whilst also building. One at a time!
-    </p>
-    <br />
+    <div class="grid grid-cols-7 gap-0">
+        <div
+            class="col-span-7 md:col-span-3 flex flex-col justify-center overflow-clip"
+        >
+            <pre
+                class="text-[0.1rem]/[0.125rem] content-center border-dotted border-2 py-1 px-2 rounded-none flex justify-center">{BOOKS_PFP}</pre>
+        </div>
 
+        <div class="col-span-7 pt-4 md:pt-0 md:col-span-4">
+            <div
+                class="md:p-4 h-full grow flex flex-col justify-center text-left"
+            >
+                <p>
+                    List of resources used (or actively using) to learn more
+                    about topics I'm interested in. Mainly CS, occassionally
+                    others. Includes articles and cool reads that I keep for
+                    reference or for sharing.
+                </p>
+                <p class="pt-3">
+                    Not listed in order of interest or priority. Anything that
+                    I'm working on has a WIP tag attached. Others have TODO. No
+                    tag means done. Balancing between building, learning, work
+                    and hobbies. Learning new cool things is fun!
+                </p>
+            </div>
+        </div>
+    </div>
+    <br />
     <h5>databases</h5>
     <ul class="list">
         <li>
@@ -42,6 +58,7 @@
     <br />
     <h5>distsys</h5>
     <ul class="list">
+        <li>WIP: Designing Data Intensive Applications</li>
         <li>
             <a
                 href="https://www.somethingsimilar.com/2013/01/14/notes-on-distributed-systems-for-young-bloods/"
